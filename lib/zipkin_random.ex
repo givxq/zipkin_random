0e856b2fefa70a23d418cb990df691da82ae5bee
david.g <david.g@kakaopaysec.com>
2022-12-19 18:02:47 +0900

    init

---
+++ lib/zipkin_random.ex
@@ -0,0 +1,18 @@
+defmodule ZipkinRandom do
+  @moduledoc """
+  Documentation for `ZipkinRandom`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZipkinRandom.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
